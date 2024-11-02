b4ca53be9d96e8c701235ba7ece6d4ae4c09d4a5
vishal7245 <vishalonceagain@gmail.com>
2024-11-03 00:12:40 +0530

    fixed links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,14 +25,14 @@ const Navbar = () => {
           
           {/* Contact Info */}
           <div className="h-full flex items-center gap-x-4 text-gray-700">
-            <a href="tel:" className="flex gap-1 text-sm" rel="noreferer">
+            <a href="tel: +91 971 772 2199" className="flex gap-1 text-sm" rel="noreferer">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M14.414 7l3.293-3.293a1 1 0 00-1.414-1.414L13 5.586V4a1 1 0 10-2 0v4.003a.996.996 0 00.617.921A.997.997 0 0012 9h4a1 1 0 100-2h-1.586z" />
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
               <span className="hidden sm:flex">+91 971 772 2199</span>
             </a>
-            <a href="mailto:" className="flex gap-1 items-center" rel="noreferer">
+            <a href="mailto: nutreyvibes@gmail.com" className="flex gap-1 items-center" rel="noreferer">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                 <path
                   fillRule="evenodd"
